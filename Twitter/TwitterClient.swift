02d216608c2ba2439719cddc9e8ef2241afc9cbb
abosadiq <wafimohamed@Wafis-MacBook-Pro.local>
2016-02-08 22:50:05 -0800

    first commit

---
+++ Twitter/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Wafi MoHamed on 2/8/16.
+//  Copyright © 2016 wafi. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+}
